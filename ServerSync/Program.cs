472131da5127113be7ee53afe51e1d10308892a6
Макс Конончук <eexet17@gmail.com>
2023-09-25 21:06:15 +0300

    Add project files.

---
+++ ServerSync/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static string address = "127.0.0.1";
+    static int port = 8080;             
+
+    static void Main(string[] args)
+    {
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        IPEndPoint remoteEndPoint = null;
+        UdpClient listener = new UdpClient(ipPoint);
+
+        try
+        {
+            Console.WriteLine("Server started! Waiting for connection...");
+
+            while (true)
+            {
+                byte[] data = listener.Receive(ref remoteEndPoint);
+
+                string msg = Encoding.Unicode.GetString(data);
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {msg} from {remoteEndPoint}");
+
+                string message = "Message was sent!";
+                data = Encoding.Unicode.GetBytes(message);
+                listener.Send(data, data.Length, remoteEndPoint);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            listener.Close();
+        }
+
+//listener.Close();
+    }
+}
---
+++ UdpHomeWork/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UdpHomeWork
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string address = string.Empty;
+        static int port = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            address = ipTxtBox.Text;
+            port = int.Parse(portTxtBox.Text);
+            ClientSync();
+        }
+        private void ClientSync()
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                EndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                string message = "";
+                while (message != "end")
+                {
+                    message = messageTxtBox.Text;
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+
+                    socket.SendTo(data, ipPoint);
+
+                    int bytes = 0;
+                    string response = "";
+                    data = new byte[1024]; 
+                    do
+                    {
+                        bytes = socket.ReceiveFrom(data, ref remoteIpPoint);
+                        response += Encoding.Unicode.GetString(data, 0, bytes);
+                    } while (socket.Available > 1);
+
+                    MessageBox.Show("server response: " + response);
+                }
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
